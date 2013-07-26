96ea5de39a1759fd6cddee53add5593df54c70db
MazeXD <mazexd@hotmail.de>
2013-07-26 18:19:06 +0200

    Added MultiHandler - automatically handle errors in handlers

--- src/webapi/constants.py
+++ src/webapi/constants.py
@@ -8,7 +8,8 @@ HTTP_PORT = 12351
 
 # error constants (common)
 ERROR_INVALID_REQUEST = -1
-ERROR_INVALID_RESPONSE = -2
+ERROR_SYSTEM = -2
+ERROR_INVALID_PLAYER = -3
 
 # error constants (http)
 ERROR_INVALID_METHOD = -20
--- src/webapi/handlers.py
+++ src/webapi/handlers.py
@@ -1,5 +1,7 @@
 from webapi.common import generate_error, generate_success, log, fullname
-from webapi.constants import ERROR_INVALID_REQUEST, ERROR_INVALID_RESPONSE
+from webapi.constants import ERROR_INVALID_REQUEST, ERROR_SYSTEM
+
+from twisted.python import log
 
 import inspect
 import json
@@ -20,26 +22,26 @@ class RequestHandler (object):
         if not issubclass(klass, Handler):
             log('Class (%s) must be a subclass of Handler' % fullname(klass))
             return
-        handles = getattr(klass, 'handles', None)
-        if not handles:
-            log('Missing handles attribute for %s' % fullname(klass))
+        accepts = getattr(klass, 'accepts', None)
+        if not accepts:
+            log('Missing accepts attribute for %s' % fullname(klass))
             return
-        if isinstance(handles, basestring):
-            handles = handles.lower()
-            if handles in self.handlers:
+        if isinstance(accepts, basestring):
+            accepts = accepts.lower()
+            if accepts in self.handlers:
                 log('%s (%s) has already a handler' % (
-                    handles, fullname(klass)))
+                    accepts, fullname(klass)))
                 return
-            self.handlers[handles] = klass
+            self.handlers[accepts] = klass
             return
-        if isinstance(handles, list):
-            for handling in handles:
-                handling = handling.lower()
-                if handling in self.handlers:
+        if isinstance(accepts, list):
+            for accept in accepts:
+                accept = accept.lower()
+                if accept in self.handlers:
                     log('%s (%s) has already a handler' % (
-                        handling, fullname(klass)))
+                        accept, fullname(klass)))
                     continue
-                self.handlers[handling] = klass
+                self.handlers[accept] = klass
 
     def parse(self, data):
         try:
@@ -65,38 +67,52 @@ class RequestHandler (object):
         if key not in handlers:
             return generate_error(ERROR_INVALID_REQUEST, key)
         handler = handlers[key](self)
-        response = handler._handle(key, request.get('data', None))
+        try:
+            response = handler._handle_internal(key, request.get('data', None))
+        except:
+            log.err()
+            return generate_error(ERROR_SYSTEM, key)
         if isinstance(response, basestring):
             return response
         if isinstance(response, dict):
             response['request'] = key
             return json.dumps(response)
-        return generate_error(ERROR_INVALID_RESPONSE, key)
+        return generate_error(ERROR_SYSTEM, key)
 
 
 class Handler (object):
-    handles = None
+    accepts = None
 
     def __init__(self, handler):
         self.handler = handler
         self.server = handler.server
 
     def error(self, error_code):
-        return generate_error(error_code, self.handling)
+        return generate_error(error_code, self.accepted)
 
     def success(self, data=None):
-        return generate_success(self.handling, data)
+        return generate_success(self.accepted, data)
 
-    def _handle(self, handling, data):
-        self.handling = handling
+    def _handle_internal(self, accepted, data):
+        self.accepted = accepted
         return self.handle(data)
 
     def handle(self, data):
         pass
 
 
+class MultiHandler (Handler):
+    def handle(self, data):
+        name = 'handle_%s' % self.accepted.lower().replace('-', '_')
+        method = getattr(self, name, None)
+        if method is None:
+            log('MultiHandler is missing %s handler method' % name)
+            return self.error(ERROR_SYSTEM)
+        return method(data)
+
+
 class PingHandler (Handler):
-    handles = 'ping'
+    accepts = 'ping'
 
     def handle(self, data):
         return self.success('pong')
